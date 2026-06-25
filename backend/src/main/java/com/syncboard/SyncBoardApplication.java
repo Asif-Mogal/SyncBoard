ca7e1b5645beb7c5fea9d88fc6dd277d2f75a854
AsifMogal2003 <mogalasif2003@gmail.com>
2026-06-26 02:00:23 +0530

    Remove diagnostic startup logging from SyncBoardApplication

--- backend/src/main/java/com/syncboard/SyncBoardApplication.java
+++ backend/src/main/java/com/syncboard/SyncBoardApplication.java
@@ -1,35 +1,12 @@
 package com.syncboard;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Value;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-public class SyncBoardApplication implements CommandLineRunner {
-    private static final Logger log = LoggerFactory.getLogger(SyncBoardApplication.class);
-
-    @Value("${spring.datasource.url}")
-    private String datasourceUrl;
-
+public class SyncBoardApplication {
     public static void main(String[] args) {
-        // Print diagnostics before starting Spring Boot
-        System.out.println("DIAGNOSTIC - Env SPRING_DATASOURCE_URL: " + maskUrl(System.getenv("SPRING_DATASOURCE_URL")));
-        System.out.println("DIAGNOSTIC - Env SPRING_DATA_REDIS_HOST: " + maskUrl(System.getenv("SPRING_DATA_REDIS_HOST")));
         SpringApplication.run(SyncBoardApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        log.info("DIAGNOSTIC: Resolved spring.datasource.url = {}", maskUrl(datasourceUrl));
-        log.info("DIAGNOSTIC: System Env SPRING_DATASOURCE_URL = {}", maskUrl(System.getenv("SPRING_DATASOURCE_URL")));
-    }
-
-    private static String maskUrl(String url) {
-        if (url == null) return "null";
-        return url.replaceAll(":[^:@/]+@", ":****@");
     }
 }
 
